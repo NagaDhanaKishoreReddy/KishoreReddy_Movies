8421aae6fe2cf81253538b1ebf8a1ae97b44ca43
DhanaKishore Reddy <dhanakishore.reddy@collabera.com>
2021-08-02 21:11:49 +0530

    Intent added for 2 screens

--- app/src/main/java/com/mvvm_withroom/adapter/MoviesAdapter.kt
+++ app/src/main/java/com/mvvm_withroom/adapter/MoviesAdapter.kt
@@ -1,11 +1,13 @@
 package com.mvvm_withroom.adapter
 
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.mvvm_withroom.MoviesDetailActivity
 import com.mvvm_withroom.R
 import com.mvvm_withroom.databinding.ItemMoviesBinding
 import com.mvvm_withroom.response.SearchMovieResponse
@@ -36,6 +38,11 @@ class MoviesAdapter(private val moviesList: SearchMovieResponse) : RecyclerView.
         holder.mBinding.tvMovieTitle.text = moviesList.results?.get(position)?.title
         holder.mBinding.tvReleaseDate.text = moviesList.results?.get(position)?.releaseDate
         holder.mBinding.tvMovieRating.text = moviesList.results?.get(position)?.voteAverage.toString()
+        holder.itemView.setOnClickListener { v ->
+            val intent = Intent(v.context, MoviesDetailActivity::class.java)
+            v.context.startActivity(intent)
+
+        }
     }
 
     inner class MoviesHolder(val mBinding: ItemMoviesBinding) :
